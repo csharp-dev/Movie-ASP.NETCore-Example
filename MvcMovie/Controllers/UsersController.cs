f9d69a32fd667f05924989d201392238a785dd32
Anit Shrestha Manandhar <codeanit@gmail.com>
2022-12-19 21:26:07 +0545

    Implement Login Feature Feature scaffolded from commandline:  https://learn.microsoft.com/en-us/aspnet/core/security/authentication/scaffold-identity

--- MvcMovie/Controllers/UsersController.cs
+++ MvcMovie/Controllers/UsersController.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 using MvcMovie.Data;
 using MvcMovie.Models;
 
 namespace MvcMovie.Controllers
-{
+{   
+    [Authorize]
     public class UsersController : Controller
     {
         private readonly MvcMovieContext _context;
